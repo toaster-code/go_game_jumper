8042a78c3baabfb6547d32f1caa0da26ddb0574d
Fabio <fabio.manzoni@gmail.com>
2023-07-30 16:07:14 +0200

    Now working with a player. moving right and left works, fluid movement. Errors in precision of the position due to missing sub pixel calculations.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 
@@ -10,31 +11,32 @@ import (
 
 // Define the variables for the rectangle position, velocity, and impulse.
 var (
-	rectX, rectY          float64 = 150, 150
-	rectWidth, rectHeight float64 = 20, 20
-	speed                 float64 = 2
-	jumpImpulse           float64 = -5 // The upward impulse when jumping.
-	gravity               float64 = 0.2
-	airControl            float64 = 0.1
-	grounded              bool    = true
-	velocityX, velocityY  float64
+	playerPosX, playerPosY    float64 = 0, 0
+	playerWidth, playerHeight float64 = 16, 16
+	maxGroundSpeed            float64 = 2
+	jumpImpulse               float64 = -5 // The upward impulse when jumping.
+	gravity                   float64 = 0.2
+	airControl                float64 = 0.1
+	isOnGround                bool    = true
+	playerVelX, playerVelY    float64 = 0, 0
 )
 
 // Get the window size.
-var (
+const (
 	windowWidth  = 320
 	windowHeight = 240
 )
 
 var (
 	// The image's dimensions
-	imageWidth, imageHeight float64 = 16, 16
-	marioImage              *ebiten.Image
+	playerImage              *ebiten.Image
 )
+
+// Game implements ebiten.Game interface.
+type Game struct{}
 
 // Define DrawImageOptions
 var opts = &ebiten.DrawImageOptions{}
-
 
 func NewImageFromImage(source *ebiten.Image) (*ebiten.Image, error) {
 	// Create a new image with the same size as the source image.
@@ -45,101 +47,115 @@ func NewImageFromImage(source *ebiten.Image) (*ebiten.Image, error) {
 	return newImage, err
 }
 
-
-
 // This function is called every frame.
-func update(screen *ebiten.Image) error {
+func (g *Game) Update(screen *ebiten.Image) error {
+
 	// Move the rectangle based on the arrow keys.
-	if grounded {
+	if isOnGround {
 		if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-			velocityX = -speed
+			playerVelX = -maxGroundSpeed
 		} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
-			velocityX = speed
+			playerVelX = maxGroundSpeed
 		} else {
 			// If no arrow keys are pressed, gradually reduce the horizontal velocity to simulate friction.
-			velocityX *= 0.90
+			playerVelX *= 0.95
 		}
-	}
+	} else {
+		// Apply gravity if the rectangle is not grounded.
+		playerVelY += gravity
 
-	// Apply gravity if the rectangle is not grounded.
-	if !grounded {
-		velocityY += gravity
-	}
 
-	// Handle jumping mechanics.
-	if grounded && ebiten.IsKeyPressed(ebiten.KeyUp) {
-		velocityY = jumpImpulse
-		grounded = false
-	}
+		// Handle jumping mechanics.
+		if ebiten.IsKeyPressed(ebiten.KeyUp) {
+			playerVelY = jumpImpulse
+			isOnGround = false
+		}
 
-	// Allow air control to add a smaller effect to the horizontal velocity while in the air.
-	if !grounded {
+		// Allow air control to add a smaller effect to the horizontal velocity while in the air.
 		if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-			velocityX -= airControl
+			playerVelX -= airControl
 		}
 		if ebiten.IsKeyPressed(ebiten.KeyRight) {
-			velocityX += airControl
+			playerVelX += airControl
 		}
-	}
-	// Allow air friction when no key is pressed.
-	if !grounded {
+
+		// Allow air friction when no key is pressed.
 		// if no arrow keys are pressed, gradually reduce the horizontal velocity to simulate friction.
 		if ebiten.IsKeyPressed(ebiten.KeyUp) && !ebiten.IsKeyPressed(ebiten.KeyLeft) && !ebiten.IsKeyPressed(ebiten.KeyRight) {
-			velocityX *= 0.90
+			playerVelX *= 0.90
 		}
 	}
 
 	// Update the rectangle's position based on velocity.
-	rectX += velocityX
-	rectY += velocityY
+	playerPosX += playerVelX
+	playerPosY += playerVelY
 
 	// Perform collision detection to prevent the rectangle from moving outside the window frame.
-	if rectX < 0 {
-		rectX = 0
-		velocityX = 0
+	if playerPosX < 0 {
+		playerPosX = 0
+		playerVelX = 0
 	}
-	if rectX+rectWidth > float64(windowWidth) {
-		rectX = float64(windowWidth) - rectWidth
-		velocityX = 0
+	if playerPosX+playerWidth > float64(windowWidth) {
+		playerPosX = float64(windowWidth) - playerWidth
+		playerVelX = 0
 	}
-	if rectY < 0 {
-		rectY = 0
-		velocityY = 0
+	if playerPosY < 0 {
+		playerPosY = 0
+		playerVelY = 0
 	}
-	if rectY+rectHeight > float64(windowHeight) {
-		rectY = float64(windowHeight) - rectHeight
-		velocityY = 0
-		grounded = true
+	if playerPosY+playerHeight > float64(windowHeight) {
+		playerPosY = float64(windowHeight) - playerHeight
+		playerVelY = 0
+		isOnGround = true
 	}
 
-	// Draw the rectangle at the updated position.
-	screen.Fill(color.Black) // Clear the screen to avoid artifacts.
-	// ebitenutil.DrawRect(screen, rectX, rectY, rectWidth, rectHeight, color.White)
-
-	// define position
-	// opts.GeoM.Scale(3, 3)
-	opts.GeoM.Translate(rectX, rectY)
-	rectX = 0
-	rectY = 0
-	screen.DrawImage(marioImage, opts)
+	// No errors occurred, return nil (zero-value).
 	return nil
 }
 
+// Draw draws the game screen.
+// Draw is called every frame (typically 1/60[s] for 60Hz display).
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Game rendering.
+	screen.Fill(color.Black) // Clear the screen to avoid artifacts.
+
+	// Print stats on the screen
+	statsText := fmt.Sprintf("Stats:\nX: %.1f\nY: %.1f\nVelocityX: %.1f\nVelocityY: %.1f\nisOngroud %t", playerPosX, playerPosY, playerVelX, playerVelY, isOnGround)
+	ebitenutil.DebugPrint(screen, statsText)
+
+	// Draw the rectangle at the updated position.
+	// ebitenutil.DrawRect(screen, rectX, rectY, rectWidth, rectHeight, color.White)
+
+	// define position
+	opts.GeoM.Translate(playerVelX, playerVelY)
+	// opts.GeoM.Translate(rectX, rectY)
+	screen.DrawImage(playerImage, opts)
+}
+
+// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
+// If you don't have to adjust the screen size with the outside size, just return a fixed size.
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return windowWidth*1.8, windowHeight*1.8
+}
+
 func main() {
+	game := &Game{}
+
 	// Create a new window with a width of 320 and a height of 240 pixels.
-	ebiten.SetWindowSize(windowWidth, windowHeight)
-	ebiten.SetWindowTitle("Move the Rectangle!")
+	ebiten.SetWindowSize(windowWidth*2, windowHeight*2)
+	ebiten.SetWindowTitle("Mario")
 
 	var err error // Declare the 'err' variable to capture the error from NewImageFromFile.
 
-	marioImage, _, err = ebitenutil.NewImageFromFile("./res/small_mario_p0.png", ebiten.FilterDefault)
+	playerImage, _, err = ebitenutil.NewImageFromFile("./res/small_mario_p0.png", ebiten.FilterDefault)
+	opts.GeoM.Scale(2, 2)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-
 	// Start the game loop.
-	if err := ebiten.Run(update, windowWidth, windowHeight, 2, "Move the Rectangle"); err != nil {
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
